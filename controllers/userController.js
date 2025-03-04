8e8ebd18fca1b25750c0fda72ca401170da28d90
EpicVN <ledangkhoahi3@gmail.com>
2025-03-04 12:13:23 +0700

    Create user authentication feature

---
+++ controllers/userController.js
@@ -0,0 +1,92 @@
+import userModel from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import validator from "validator";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
+// Route for user login
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      res.status(400).json({ success: false, message: "User not found!" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      res.status(400).json({ success: false, message: "Invalid credentials!" });
+    } else {
+      const token = createToken(user._id);
+
+      res.json({ success: true, token });
+    }
+  } catch (error) {
+    console.log(error);
+    res, json({ success: true, message: error.message });
+  }
+};
+
+// Route for user register
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if user already exists
+    const userExists = await userModel.findOne({ email });
+
+    if (userExists) {
+      res.status(400).json({ success: false, message: "User already exists!" });
+    }
+
+    // Validate email format & password
+    if (!validator.isEmail(email)) {
+      res
+        .status(400)
+        .json({ success: false, message: "Please enter a valid email" });
+    }
+
+    if (password.length < 8) {
+      res.status(400).json({
+        success: false,
+        message: "Password must be at least 8 characters long",
+      });
+    }
+
+    // Hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create new user
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+
+    res.json({
+      success: true,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res, json({ success: true, message: error.message });
+  }
+};
+
+// Route for admin login
+const loginAdmin = async (req, res) => {
+  res.json({ msg: "Register API working!" });
+};
+
+export { loginUser, registerUser, loginAdmin };
